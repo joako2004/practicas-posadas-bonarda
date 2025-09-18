79aec0db44bd87f0f0ad086f0a9f8eb751daa5ac
joaquin <joakoperaltacorbellini@gmail.com>
2025-09-18 08:03:20 -0300

    modificación de css, html y creacion de archivo js para corroboración de contraseñas iguales

---
+++ public/assets/js/crear_cuenta.js
@@ -0,0 +1,10 @@
+document.querySelector('.registration-form').addEventListener('submit', function(e) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    
+    if (password !== confirmPassword) {
+        e.preventDefault();
+        alert('Las contraseñas no coinciden');
+        return false;
+    }
+});
